66f0dd79dd5a89ac92f89987d1a9cf30c29ce680
Lam Thanh Hoai <lamthanhhoai1990@gmail.com>
2018-04-14 07:59:03 +0700

    Blink su dung setState, setInterval

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
-import {View, Text,} from 'react-native';
+import { Blink } from './Blink';
+import {
+  View, Text, 
+} from 'react-native';
+
 export class App extends Component {
   render() {
     return (
       <View>
-        <Text>
-          Hello World!
-        </Text>
-        <World name="Lam Thanh Hoai"></World>
-      </View>      
+        <Blink text="Day la Text Blink"></Blink>
+      </View>
     )
   }
-}
-
-class World extends Component {
-  render() {
-    return (
-      <Text>Xin chao, toi ten la {this.props.name}</Text>
-    )
-}
 }
